48d2b92a9e4230ee2ed4aba12ca0ccf5f63366af
hklemp <hendrik.klemp@kumavision.de>
2018-05-08 20:32:25 +0200

    Initial commit

---
+++ UriExtension.Test/UriBuilderExtensionsTest.cs
@@ -0,0 +1,23 @@
+using System;
+using UriBuilderExtension;
+using Xunit;
+
+namespace UriExtension.Test
+{
+	public class UriBuilderExtensionsTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            UriBuilder uriBuilder = new UriBuilder();
+            uriBuilder.Scheme = "http";
+            uriBuilder.Host = "www.google.com";
+            uriBuilder.Path = "search";
+
+            uriBuilder.SetQueryParam("q", "query+goes+here");
+
+            Assert.Equal("http://www.google.com/search?q=query+goes+here", uriBuilder.Uri.ToString());
+
+        }
+    }
+}
---
+++ UriExtension/UriBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace UriBuilderExtension
+{
+	public static class UriBuilderExtensions
+    {
+
+        /// <summary>
+        /// Sets the specified query parameter key-value pair of the URI.
+        /// If the key already exists, the value is overwritten.
+        /// </summary>
+        public static UriBuilder SetQueryParam(this UriBuilder uri, string key, Microsoft.Extensions.Primitives.StringValues value)
+        {
+            
+            var queryDictionary = ParseQuery(uri.Query);
+
+            queryDictionary.Add(key, value);
+
+           
+            uri.Query = BuildQuery(queryDictionary);
+
+            return uri;
+        }
+
+
+        private static string BuildQuery(Dictionary<string, Microsoft.Extensions.Primitives.StringValues> dictionary)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("?");
+
+            foreach (var item in dictionary)
+            {
+                sb.AppendFormat("{0}={1}&", item.Key, item.Value);
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+
+        private static Dictionary<string,Microsoft.Extensions.Primitives.StringValues> ParseQuery(string queryString)
+        {
+            return QueryHelpers.ParseQuery(queryString);
+        }
+    }
+}
